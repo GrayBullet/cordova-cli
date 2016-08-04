d7726a3cad82c645a6d05ebbf5124fd493cf4cf8
Tomo Masakura <tomo.masakura@gmail.com>
2016-08-04 14:01:48 +0900

    Auto generator .cordova-clirc when `cordova create`

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var cordova = require('./libs/cordova-spawn');
 var optionsFactory = require('./libs/options-loader');
+var cordovaArguments = require('./libs/cordova-arguments');
 
 var invoke = function (args) {
   var cliOptions = optionsFactory.load();
@@ -10,7 +11,18 @@ var invoke = function (args) {
     cwd: cliOptions['cordova-root']
   };
 
-  return cordova(args, options);
+  return cordova(args, options)
+    .then(function () {
+      if (cordovaArguments.isCreate(args)) {
+        var root = cordovaArguments.getRoot(args);
+
+        // Create .cordova-clirc and set `cordova-root`.
+        var fs = require('fs');
+        fs.writeFile('.cordova-clirc', JSON.stringify({
+          'cordova-root': root
+        }), 'utf8');
+      }
+    });
 };
 
 module.exports = invoke;
