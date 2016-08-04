56ccf635df9df17ff3fcd78615049f93a34fd708
Tomo Masakura <tomo.masakura@gmail.com>
2016-08-04 10:25:08 +0900

    Extract project root searching

--- index.js
+++ index.js
@@ -3,12 +3,52 @@
 var cordova = require('./libs/cordova-spawn');
 var optionsFactory = require('./libs/options-loader');
 
-module.exports = function (args) {
+function parseCordovaArgs(args) {
+  var params = args.filter(function (a) { return a.trim().match(/^-/); })
+  var options = args.filter(function (a) { return !a.trim().match(/^-/); })
+
+  var command = params.shift();
+
+  return {
+    command: command,
+    params: params,
+    options: options
+  };
+}
+
+var invoke = function (args) {
   var cliOptions = optionsFactory.load();
 
   var options = {
     cwd: cliOptions['cordova-root']
   };
 
-  return cordova(args, options);
+  return cordova.fromAuto()
+    .then(function (cmd) {
+      return cmd(args, options);
+    })
+    .then(function (code) {
+      console.log(code);
+    });
 };
+
+invoke.extra = function (args) {
+  var cliOptions = optionsFactory.load();
+
+  var options = {
+    cwd: cliOptions['cordova-root']
+  };
+
+  var cordovaArgs = parseCordovaArgs(args);
+  if (cordovaArgs.command === 'copy-dist') {
+    var platforms = cordovaArgs.params;
+
+    if (_.includes(platforms, 'android')) {
+      path.join(cliOptions['cordova-root'], 'platforms', 'android', 'build', 'outputs', 'apk');
+    }
+  }
+
+  return cordova.extra(args, options);
+};
+
+module.exports = invoke;
--- libs/cordova-spawn.js
+++ libs/cordova-spawn.js
@@ -3,6 +3,7 @@
 var spawn = require('child_process').spawn;
 var path = require('path');
 var _ = require('underscore');
+var ProjectRootSearcher = require('./project-root-searcher');
 
 function cordova(args, optOptions) {
   return new Promise(function (resolve) {
@@ -17,5 +18,35 @@ function cordova(args, optOptions) {
       });
   });
 }
+
+function getCmd() {
+  return ProjectRootSearcher.search()
+    .then(function (dir) {
+      return path.join(dir.bin, 'cordova')
+    });
+}
+
+cordova.fromPath = function (cmd) {
+  return function (args, optOptions) {
+    return new Promise(function (resolve, reject) {
+      var options = _.assign({}, {env: process.env, stdio: 'inherit'}, optOptions);
+
+      try {
+        spawn(cmd, args, options)
+          .on('close', resolve);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    });
+  };
+};
+
+cordova.fromAuto = function () {
+  return getCmd()
+    .then(function (cmd) {
+      return cordova.fromPath(cmd);
+    });
+};
 
 module.exports = cordova;
