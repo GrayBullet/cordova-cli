1baa71344474e439b81a3fd0da8763b29d6fe1d0
Tomo Masakura <tomo.masakura@gmail.com>
2016-08-05 08:38:31 +0900

    Extract cordova-cli-lib

--- libs/options/default-options-loader.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var loader = {
-  load: function () {
-    return {};
-  }
-};
-
-module.exports = loader;
--- libs/promise.js
+++
@@ -1,9 +0,0 @@
-/** @namespace Promise */
-/** @namespace Promise.then */
-/** @namespace Promise.catch */
-/** @namespace Promise.resolve */
-/** @namespace Promise.reject */
-/** @namespace Promise.all */
-/** @namespace Promise.race */
-
-module.exports = Promise;
